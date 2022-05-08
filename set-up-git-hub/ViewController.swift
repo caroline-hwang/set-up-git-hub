f042601c43ad6df72e3bd742073e5fb956260a01
carolinehwang13 <caroline_hwang@brown.edu>
2022-05-08 00:33:07 -0400

    Initial Commit

---
+++ set-up-git-hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  set-up-git-hub
+//
+//  Created by Caroline Hwang on 5/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ set-up-git-hubUITests/set_up_git_hubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  set_up_git_hubUITestsLaunchTests.swift
+//  set-up-git-hubUITests
+//
+//  Created by Caroline Hwang on 5/8/22.
+//
+
+import XCTest
+
+class set_up_git_hubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
